5bfe9902a2b4742906ba901f4155c0666a607262
Manas <manasdeshpande@outlook.com>
2022-11-01 18:28:53 +0530

    Second

--- books/urls.py
+++ books/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('signup/',views.signup,name='signup'),
     path('login/add_user/',views.add_user,name='add_user'),
     path('signup/check_user/',views.check_user,name='check_user'),
+    path('login/add_user/buyer/',views.buyer,name='buyer'),
+    path('signup/check_user/buyer/',views.buyer,name='buyer'),
+    path('login/add_user/seller/',views.seller,name='seller'),
+    path('signup/check_user/seller/',views.seller,name='seller'),
 ]
--- books/views.py
+++ books/views.py
@@ -4,19 +4,23 @@ from django.template import loader
 import mysql.connector
 from django.urls import reverse
 from django.shortcuts import redirect
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="1234",
-    database="bookio"
-    )
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="1234",
+        database="bookio"
+        )
+except mysql.connector.Error as error:
+    print("Failed to create table in MySQL: {}".format(error))
 def mysql():
 
 
     mycursor = mydb.cursor()
     #mycursor.execute("CREATE TABLE users (name VARCHAR(255), address VARCHAR(255),username varchar(10) primary key,email varchar(20),phone_no varchar(10),password varchar(8))")
     #mycursor.execute("CREATE TABLE book (name VARCHAR(255),book_id varchar(15) primary key,publication varchar(50),author varchar(20),conditions varchar(10),mrp int,selling_price int)")
-    #mycursor.execute("CREATE TABLE users ()")
+    #mycursor.execute("CREATE TABLE buyers (buyer_name VARCHAR(255),book_name varchar(255),price int)");
+    mycursor.execute("CREATE TABLE sellers (seller_name VARCHAR(255),book_name varchar(255),b_id varchar(15) primary key,publication varchar(50),author varchar(20),conditions varchar(10),mrp int,selling_price int)")
 mysql()
 
 
@@ -46,8 +50,10 @@ def check_user(request):
         'greeting' : aa,
         'name':a
     }
-    #return render(request, 'myfirst.html', context)
-    return HttpResponse(template.render(context,request))
+    #return HttpResponseRedirect(reverse('index1'),context)
+
+    return render(request, 'myfirst.html',context)
+
 
 
 def add_user(request):
@@ -67,7 +73,6 @@ def add_user(request):
         'greeting': 1,
         'name' :a   
     }
-    #return HttpResponseRedirect(reverse('books/index'))
     return HttpResponse(template.render(context,request))
 
 
@@ -92,3 +97,25 @@ def tp(a,e):
         else:
             return 0
 
+def buyer(request):
+    return HttpResponse("Hello world! Buyer")
+
+def seller(request):
+    return HttpResponse("Hello world! Seller")
+    
+    
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
+
+def insertimage(loc):
+    add_image=mydb.cursor()
+    empPicture = convertToBinaryData(loc)
+    q1="insert into picture values(%s,%s)"
+    tup=(2,empPicture)
+    add_image.execute(q1,tup)
+    mydb.commit()
+
+#insertimage(r"C:\Users\Admin\Pictures\Screenshots\Screenshot (1)")
