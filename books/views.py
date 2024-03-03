d592e61aa08297dfeb15aa3a6cadc81494b4052f
anushka-skyovi <Anushka.Deshmukh@skyovi.com>
2024-03-03 10:51:06 +0530

    first commit

--- books/views.py
+++ books/views.py
@@ -1,29 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
-import mysql.connector
+import mysql.connector #--------------neeche connctor
 from django.urls import reverse
 from django.shortcuts import redirect
 users=""
 try:
-    mydb = mysql.connector.connect(
+    mydb = mysql.connector.connect( #storing the connection
         host="localhost",
         user="root",
-        password="1234",
-        database="bookio"
+        password="Anushka",
+        database="bokio"
         )
+    print(mydb)
     mydb1 = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="1234",
-        database="bookio"
+        password="Anushka",
+        database="bokio"
         )
 except mysql.connector.Error as error:
     print("Failed to create table in MySQL: {}".format(error))
 def mysql():
 
 
-    mycursor = mydb.cursor()
+
+    mycursor = mydb.cursor() #create mydb cha cursor 
+
     #mycursor.execute("CREATE TABLE users (name VARCHAR(255), address VARCHAR(255),username varchar(10) primary key,email varchar(20),phone_no varchar(10),password varchar(8))")
     #mycursor.execute("CREATE TABLE book (name VARCHAR(255),book_id varchar(15) primary key,publication varchar(50),author varchar(20),conditions varchar(10),mrp int,selling_price int,year varchar(50),branch varchar(50))")
     #mycursor.execute("CREATE TABLE buyers (buyer_name VARCHAR(255),book_name varchar(255),price int)");
@@ -38,7 +41,7 @@ def mysql():
     #mycursor.execute("insert into book (name, branch,year,book_id,publication,author,conditions,mrp,selling_price) values('Chemistry','FE','1',7,'Pearson','Kurose','Excellent',500,250)")
     #mycursor.execute("insert into book (name, branch,year,book_id,publication,author,conditions,mrp,selling_price) values('Physics','FE','1',8,'Thomas','Sudarshan','Good',500,250)")
     mydb.commit()
-mysql()
+#mysql()
 
 
 def index1(request):
@@ -57,12 +60,12 @@ def signup(request):
     template = loader.get_template('signup.html')
     return HttpResponse(template.render())
 
-def check_user(request):
+def check_user(request): #check if valid
     template = loader.get_template('main.html')
     a = request.POST['username']
     get_user(a)
     e = request.POST['psw']
-    aa=tp(a,e)
+    aa=tp(a,e)  #call tp neeche
     #print(aa)
     fe=mydb.cursor()
     fe1=mydb1.cursor()
@@ -86,7 +89,7 @@ def get_user(q):
     global users
     users=q
 
-def add_user(request):
+def add_user(request): #request the fields
     template = loader.get_template('main.html')
     a = request.POST['username']
     get_user(a)
@@ -98,9 +101,9 @@ def add_user(request):
     insert_user=mydb.cursor()
     sql = "INSERT INTO users (name, address,username,email,phone_no,password) VALUES (%s,%s,%s,%s,%s,%s)"
     val=(b,f,a,c,d,e)
-    insert_user.execute(sql,val)
-    mydb.commit()
-    fe1=mydb1.cursor()
+    insert_user.execute(sql,val) #insert,what to pass in %s
+    mydb.commit() #commit changes in database or wont go
+    fe1=mydb1.cursor() #fe1 cursor by mydb1 object
     fe1.execute("select * from book")
     
     rs1=fe1.fetchall()
@@ -116,14 +119,14 @@ def add_user(request):
 
 
 
-def tp(a,e):
+def tp(a,e): #from above , we have manually used select, fetchall->all values for parameter passsed, execute->select statement
     tp=mydb.cursor()
     tp1="SELECT username FROM users WHERE username = %s"
     us_nm=(a,)
-    tp.execute(tp1,us_nm)
+    tp.execute(tp1,us_nm) #executing the actual query of mysql select
     myresult = tp.fetchall()
     #print(myresult)
-    if len(myresult)==0:
+    if len(myresult)==0: #if no user in usr table, error,prompt
         return 0
     else:
         tp1="SELECT password FROM users WHERE username = %s"
@@ -131,7 +134,7 @@ def tp(a,e):
         tp.execute(tp1,us_nm)
         myresult = tp.fetchall()
         #print(myresult,e)
-        if myresult[0][0]==e:
+        if myresult[0][0]==e: #check for pwd
             return 1
         else:
             return 0
